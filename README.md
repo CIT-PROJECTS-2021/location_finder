55272272213843d89c313354f28953b105017537
Shawal77 <mbalireshawal@gmail.com>
2022-09-27 13:26:24 +0300

    12

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # location_finder
+
+```
+pip install geolocation-python
+pip install phonenumbers
+pip install geoip2
+
+pip install geocoder
+pip install folium
+```
---
+++ ipmain.py
@@ -0,0 +1,14 @@
+import geocoder
+import folium
+
+g=geocoder.ip('me')
+print(g)
+
+my_line_address=g.latlng
+print(my_line_address)
+
+my_map1=folium.Map(
+    location=my_line_address,
+    zoom_start=12
+)
+my_map1
