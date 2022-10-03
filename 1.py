669474318d4ff3c1bf5af6d4266a3ec9632d91eb
Shawal77 <mbalireshawal@gmail.com>
2022-10-04 01:52:54 +0300

    deleted unecessary files

--- 1.py
+++
@@ -1,8 +0,0 @@
-from tkinterhtml import HtmlFrame
-import tkinter as tk
-
-root = tk.Tk()
-frame = HtmlFrame(root,horizontal_scrollbar='auto',vertical_scrollbar='auto')
-frame.set_content('.index.html')
-#frame.set_content(urllib.request.urlopen)
-root.mainloop()
--- main.py
+++
@@ -1,9 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder
-import geolocation
-
-NUMBER = '+2567055088051'
-
-ch_number=phonenumbers.parse(NUMBER,'CH')
-#print(geocoder.description_for_number(ch_number,'en'))
-print(dir(geolocation))
